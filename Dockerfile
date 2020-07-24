4343402587ff2d61a1f65842c57936479162a7d5
Relms12345 <relms@furcordapp.com>
2020-07-24 11:33:10 -0700

    Switched to papermc for better performance

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,9 @@
 ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT 1
 
 COPY ./Server/bin/Release/netcoreapp5.0/publish /usr/src/code
-COPY ./start-server.sh /usr/src/startup/start-server.sh
-COPY ./eula.txt /usr/src/prestart/eula.txt
-
-RUN mkdir /usr/src/Build
-RUN curl -o /usr/src/Build/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-
-WORKDIR /usr/src/Build
-
-RUN java -jar  /usr/src/Build/BuildTools.jar --rev 1.16.1
+COPY ./mc-files/start-server.sh /usr/src/startup/start-server.sh
+COPY ./mc-files/eula.txt /usr/src/prestart/eula.txt
+COPY ./mc-files/paper.jar /usr/src/jar/paper.jar
 
 WORKDIR /usr/src/server
 
---
+++ mc-files/start-server.sh
@@ -0,0 +1,43 @@
+﻿#!/bin/bash
+
+if [ -z "$(ls .)" ];
+  then
+    echo Target dir is empty... Generating required files...
+    cd /usr/src/code
+    chmod +x ./Server
+    ./Server
+    cd /usr/src/server
+    mv /usr/src/jar/paper.jar /usr/src/server/paper.jar
+    mv /usr/src/code/server.properties /usr/src/server/server.properties
+    mv /usr/src/prestart/eula.txt /usr/src/server/eula.txt
+    echo Done
+else
+  if [ -f paper.jar ];
+    then
+      echo Found PaperMC file...
+  else
+    echo PaperMC file not found... moving file to target dir...
+    mv /usr/src/jar/paper.jar /usr/src/server/paper.jar
+    echo Done
+  fi
+
+  cd /usr/src/code
+  chmod +x ./Server
+  ./Server
+  cd /usr/src/server
+  
+  if [ -f server.properties ];
+    then
+      echo Regenerating server.properties file...
+      rm server.properties
+      mv /usr/src/code/server.properties /usr/src/server/server.properties
+      echo Done
+  else
+    echo sever.properties file not found! Moving to target dir...
+    mv /usr/src/code/server.properties /usr/src/server/server.properties
+    echo Done
+  fi
+fi
+
+echo Prechecks complete. Starting Minecraft server now.
+java -Xms1G -Xmx2G -jar paper.jar
